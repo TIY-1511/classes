38ec8aaaff38fd778addd9277ab33b08fdb8b7a2
Daryn Holmes <daryn.holmes@net-a-porter.com>
2015-11-18 20:37:27 +0000

    Added a gitignore file

--- main.rb
+++ main.rb
@@ -1,11 +1,26 @@
 
 require_relative('./rectangle')
 
-widths = [10,50,5,10] # Array.new
-heights = [20,10,5,10]
+# widths = [10,50,5,10] # Array.new
+# heights = [20,10,5,10]
 
-rect = Rectangle.new
+rect = Rectangle.new(1,9)
+rect2 = Rectangle.new(5,10)
 
-area = rect.reactangle_area(widths[3],heights[3])
+puts rect.area()
 
-puts area
+rectangles = [rect, rect2, Rectangle.new(10,90)]
+
+# rect.set_width(10)
+# rect.set_height(30)
+
+rect2.set_width(20)
+rect2.set_height(10)
+
+rectangles[2].set_width(20)
+rectangles.last.set_height(20)
+
+
+area = rect.area()
+
+rectangles.each { |r| puts r.area()}
--- rectangle.rb
+++ rectangle.rb
@@ -1,8 +1,26 @@
 
 class Rectangle
 
-  def reactangle_area(width, height) 
-    width * height
+  def initialize(width, height) 
+    puts "Hello from the constructor"
+    @width = width
+    @height = height
   end
+
+  def set_width(w) 
+    @width = w 
+  end
+
+  def set_height(h) 
+    @height = h
+  end
+
+  def area() 
+    @width * @height
+  end
+
+  # def area(width, height) 
+  #   width * height
+  # end
 
 end
