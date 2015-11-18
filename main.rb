f0aef77b24d98143a64326ad53f684bcc91b9c2f
Daryn Holmes <daryn.holmes@net-a-porter.com>
2015-11-18 19:57:35 +0000

    Using method from the class

--- main.rb
+++ main.rb
@@ -1,11 +1,11 @@
 
 require_relative('./rectangle')
 
-widths = [10,50,5,10]
+widths = [10,50,5,10] # Array.new
 heights = [20,10,5,10]
 
+rect = Rectangle.new
 
-
-area = reactangle_area(widths[3],heights[3])
+area = rect.reactangle_area(widths[3],heights[3])
 
 puts area
